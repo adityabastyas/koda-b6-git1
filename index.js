4523f5d941c7186a5ab0517622f4fffdd8228fa0
adityabastyas <adityabastyasmulya@gmail.com>
2026-01-05 23:09:47 +0700

    fix: update file index.js celcius to kelin to reamur to fahrenheit interactive

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import { celciusToFahrenheit } from "./celciusToFahrenheit.js";
 import { celciusToKelvin } from "./celciusToKelvin.js";
 import { celciusToReamur } from "./celciusToReamur.js";
 
-async function index() {
+process.stdout.write("Masukkan suhu dalam celcius: ");
+
+process.stdin.on("data", async (data) => {
   try {
-    const fahrenheit = await celciusToFahrenheit(5);
-    const kelvin = await celciusToKelvin(8);
-    const reamur = await celciusToReamur(10);
+    const input = Number(data.toString().trim());
+
+    const fahrenheit = await celciusToFahrenheit(input);
+    const kelvin = await celciusToKelvin(input);
+    const reamur = await celciusToReamur(input);
     console.log("Hasil konversi suhu celcius ke fahrenheit adalah", fahrenheit);
     console.log("Hasil konversi suhu celcius ke kelvin adalah", kelvin);
     console.log("Hasil konversi suhu celcius ke reamur adalah", reamur);
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-}
 
-index();
+    process.exit();
+  } catch (error) {
+    console.log("Error: ", error.message);
+    process.exit();
+  }
+});
